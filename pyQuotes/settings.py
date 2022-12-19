085315a2cd40f90dd2ecac75528b63bedafe0f36
maish79 <maishsupa@yahoo.com>
2022-12-19 21:05:03 +0000

    deployment commit

--- pyQuotes/settings.py
+++ pyQuotes/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quotes-main.heroku.app.com', 'localhost']
+ALLOWED_HOSTS = ['quotes-main.herokuapp.com', 'localhost']
 
 
 # Application definition
